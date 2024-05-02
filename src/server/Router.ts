811ecd798d2d32b0ff6717685dc4400de72989d4
lucavini <lucass.lv.lv@gmail.com>
2024-05-02 13:35:19 -0300

    feat: create routes file

--- src/server/Router.ts
+++ src/server/Router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import ServiceController from './controller/ServiceController';
+import ServiceController from './app/controller/ServiceController';
 
 const route = Router();
 route.get('/', ServiceController.requestService);
