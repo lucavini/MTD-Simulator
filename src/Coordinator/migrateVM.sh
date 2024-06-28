8e762ed25d5ec0077c39108b6f6d03eb434e0df2
lucavini <lucass.lv.lv@gmail.com>
2024-06-28 09:48:01 -0300

    refactor: remove coordinator folder

--- src/Coordinator/migrateVM.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-vm_origin=$1
-vm_destiny=$2
-
-root="77148816"
-
-echo "$root" | sudo -S cp "/home/lucas/Documents/git/MTD/src/Coordinator/migation_image/UbuntuVM0${vm_origin}.qcow2" "/home/lucas/Documents/git/MTD/src/Coordinator/migation_image/UbuntuVM0${vm_destiny}.qcow2"
