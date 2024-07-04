44d09b20ccf517f6c0ff290a931fce621bd904b7
lucavini <lucass.lv.lv@gmail.com>
2024-07-04 16:59:32 -0300

    feat: add app url as environment variable

--- src/Server/services/serverServiceController.ts
+++ src/Server/services/serverServiceController.ts
@@ -3,11 +3,12 @@ import axios from 'axios';
 import debug from '@Lib/Debug';
 
 const appPort = process.env.APP_PORT;
+const appURL = process.env.APP_URL;
 
 class ServerServiceController {
   async requestService(req: Request, res: Response) {
     try {
-      const response = await axios.get(`http://localhost:${appPort}/app`);
+      const response = await axios.get(`http://${appURL}:${appPort}/app`);
       debug.info('ServerServiceController', 'request redirected successfully');
       res.json(response.data);
     } catch (err) {
