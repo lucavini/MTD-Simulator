920b30604f1c53740324aa0bb04d28960587ded1
lucavini <lucass.lv.lv@gmail.com>
2024-07-04 17:00:01 -0300

    feat: update vm creation url

--- src/Server/controller/scripts/createVM.sh
+++ src/Server/controller/scripts/createVM.sh
@@ -5,12 +5,12 @@ directoryPath=$2
 
 root="77148816"
 mkdir -p /home/lucas/Documents/git/MTD/src/Coordinator/migation_image
-echo "$root" | sudo -S cp "/var/lib/libvirt/images/UbuntuVM01.qcow2" "/home/lucas/Documents/git/MTD/src/Coordinator/migation_image/UbuntuVM0${vm_version}.qcow2"
+echo "$root" | sudo -S cp "/var/lib/libvirt/images/UbuntuVM01.qcow2" "/home/lucas/Documents/git/MTD/src/Server/controller/migation_image/UbuntuVM0${vm_version}.qcow2"
 
 virt-install --name="UbuntuVM0$vm_version" \
 --vcpus=2 \
 --memory=2024 \
---disk path="/home/lucas/Documents/git/MTD/src/Coordinator/migation_image/UbuntuVM0${vm_version}.qcow2",format=qcow2 \
+--disk path="/home/lucas/Documents/git/MTD/src/Server/controller/migation_image/UbuntuVM0${vm_version}.qcow2",format=qcow2 \
 --import \
 --check path_in_use=off \
 --disk size=10
