9c671641eb47168bbdba3b85789eb514d5ad543f
lucavini <lucass.lv.lv@gmail.com>
2024-07-04 18:43:00 -0300

    feat: create client socket request

--- src/App/index.ts
+++ src/App/index.ts
@@ -1,10 +1,12 @@
 import '../../path.config';
 import express from 'express';
 import debug from '@Lib/Debug';
+import io from 'socket.io-client';
 import route from './Router';
 
 const app = express();
 const port = process.env.APP_PORT;
+const socketPort = process.env.SOCKET_PORT;
 
 app.use(express.json());
 app.use(route);
@@ -12,3 +14,6 @@ app.use(route);
 app.listen(port, () => {
   debug.info('🚀 App', `is running on http://localhost:${port}`);
 });
+
+const socket = io(`http://192.168.0.105:${socketPort}`);
+socket.emit('App running', 'App has started');
