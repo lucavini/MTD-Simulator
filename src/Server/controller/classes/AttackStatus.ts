ebbcd18e9808bec02e43d2a017e0a0b08a0b1883
lucavini <lucass.lv.lv@gmail.com>
2024-09-17 10:27:52 -0300

    feat: add attack stages and time per stage attributes

--- src/Server/controller/classes/AttackStatus.ts
+++ src/Server/controller/classes/AttackStatus.ts
@@ -1,33 +1,56 @@
-import debug from '@Lib/Debug';
-
 class AttackStatus {
   private static attackStages: number;
 
-  private static attackProgress: number;
+  private CurrentAttackStage: number;
+
+  private static TimePerStage: number;
+
+  private CurrentAttackTime: number;
 
   private static isVMCompromised: boolean;
 
-  public constructor(attackProgress: number) {
-    AttackStatus.attackProgress = attackProgress;
+  public constructor() {
     AttackStatus.attackStages = Number(process.env.ATTACK_STAGES);
+    this.CurrentAttackStage = 0;
+    AttackStatus.TimePerStage = Number(process.env.TIME_PER_STAGE);
+    this.CurrentAttackTime = 0;
     AttackStatus.isVMCompromised = false;
-    debug.info('AttackStatus', 'instance created');
   }
 
   public get attackStages() {
     return AttackStatus.attackStages;
   }
 
+  public get currentAttackStage() {
+    return this.CurrentAttackStage;
+  }
+
+  public increaseAttackStage() {
+    this.CurrentAttackStage += 1;
+  }
+
+  public get timePerStage() {
+    return AttackStatus.TimePerStage;
+  }
+
+  public get currentAttackTime() {
+    return this.CurrentAttackTime;
+  }
+
+  public increateCurrentAttackTime() {
+    this.CurrentAttackTime += 1;
+  }
+
+  public resetCurrentAttackTime() {
+    this.CurrentAttackTime = 0;
+  }
+
   public get isVMCompromised() {
     return AttackStatus.isVMCompromised;
   }
 
-  public get attackProgress() {
-    return AttackStatus.attackProgress;
-  }
-
-  public set increateAttackProgress(progress: number) {
-    AttackStatus.attackProgress += progress;
+  public setIsVMCompromised() {
+    AttackStatus.isVMCompromised = true;
   }
 }
 
