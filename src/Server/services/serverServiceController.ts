02f452bb3ad28552e80be30fe25129b5407d406e
lucavini <lucass.lv.lv@gmail.com>
2024-09-17 10:27:02 -0300

    feat: block requests if vm is compromised

--- src/Server/services/serverServiceController.ts
+++ src/Server/services/serverServiceController.ts
@@ -1,18 +1,25 @@
 import { Request, Response } from 'express';
 import axios from 'axios';
-import debug from '@Lib/Debug';
+// import debug from '@Lib/Debug';
+import TimeModule from '../modules/timeModule';
 
 const appPort = process.env.APP_PORT;
 const appURL = process.env.APP_URL;
 
 class ServerServiceController {
   async requestService(req: Request, res: Response) {
+    if (TimeModule.currentVm?.status?.isVMCompromised) {
+      return res.status(400).json({
+        error: 'error',
+      });
+    }
+
     try {
       const response = await axios.get(`http://${appURL}:${appPort}/app`);
-      debug.info('ServerServiceController', 'request redirected successfully');
-      res.json(response.data);
+      // debug.info('ServerServiceController', 'request redirected successfully');
+      return res.json(response.data);
     } catch (err) {
-      res.status(400).json({
+      return res.status(400).json({
         error: err,
       });
     }
